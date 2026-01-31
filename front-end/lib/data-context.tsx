411c498383ac931c2469ea393bac73cb8677cbbe
Suhyun444 <windseed71@gmail.com>
2026-02-01 01:21:02 +0900

    fix : basepath 추가 이후 경로 오류 해결

--- front-end/lib/data-context.tsx
+++ front-end/lib/data-context.tsx
@@ -45,6 +45,7 @@ export function DataProvider({ children }: { children: React.ReactNode }) {
         setIsAuthChecked(true)
       } catch (error) {
         console.error("DataProvider: Login check failed, redirecting...", error)
+        router.push("/login/google")
       }
     }
 
